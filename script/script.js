3c706026466b4a6150e0e03abd5a75563ecb6cd4
Maia Bell <maiarose04@gmail.com>
2022-11-08 13:17:05 -0800

    Asks user to enter a 4 digit number

--- script/script.js
+++ script/script.js
@@ -2,4 +2,11 @@ let course1 = {code: "ACIT 1620", coursename: "Web Tech", grade: "none"};
 let course2 = {code: "ACIT 1630", coursename: "Database", grade: "none"};
 let course3 = {code: "ACIT 1420", coursename: "Systems Admin", grade: "none"};
 
-let courselist = [course1, course2, course3];
+let courselist = [{course1}, {course2}, {course3}];
+
+console.log(courselist);
+
+
+do{
+    input = prompt("Enter a 4 digit number!")}
+    while(isNaN(input) == true || input.toString().length > 4 || input.toString().length < 4);
