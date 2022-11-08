f3b2f486344563f9bed7e5821fd00ae2c017e781
Maia Bell <maiarose04@gmail.com>
2022-11-08 12:37:52 -0800

    Adds courselist array

--- script/script.js
+++ script/script.js
@@ -1,1 +1,5 @@
-let courselist = {code: "ACIT 1620", coursename: "Fundimental Web Technologies"}
+let course1 = {code: "ACIT 1620", coursename: "Web Tech", grade: "none"};
+let course2 = {code: "ACIT 1630", coursename: "Database", grade: "none"};
+let course3 = {code: "ACIT 1420", coursename: "Systems Admin", grade: "none"};
+
+let courselist = [course1, course2, course3];
