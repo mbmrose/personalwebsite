33bb2774398b9932e221c7b2f4ffce58529141e9
Maia Bell <maiarose04@gmail.com>
2022-11-11 17:04:46 -0800

    Adds for loop and finishes question 3

--- script/script.js
+++ script/script.js
@@ -1,12 +1,27 @@
-let course1 = {code: "ACIT 1620", coursename: "Web Tech", grade: "none"};
-let course2 = {code: "ACIT 1630", coursename: "Database", grade: "none"};
-let course3 = {code: "ACIT 1420", coursename: "Systems Admin", grade: "none"};
+let course1 = {code: "1620", coursename: "Web Tech", grade: "none"};
+let course2 = {code: "1630", coursename: "Database", grade: "none"};
+let course3 = {code: "1420", coursename: "Systems Admin", grade: "none"};
 
-let courselist = [{course1}, {course2}, {course3}];
+let courselist = [course1, course2, course3];
 
 console.log(courselist);
 
+let input = null;
 
 do{
     input = prompt("Enter a 4 digit number!")}
     while(isNaN(input) == true || input.toString().length > 4 || input.toString().length < 4);
+
+
+let check = false;
+for (let mycourses of courselist){
+    if (mycourses.code.includes(input)){
+    console.log(`Yes, I am taking the course: ACIT - ${mycourses.code}, ${mycourses.coursename}`)
+    check = true;
+    }
+}
+
+if (check === false){
+    courselist.push({code: `ACIT ${input}`, coursename: null})
+    console.log(`Object '${input}' added to courselist`)
+}
